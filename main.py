f4fefdd9f1cba7946a09b88dd57d2c3bca97ed88
K-Grachev-2106756 <m2106756@edu.misis.ru>
2023-12-18 03:07:05 +0300

    Outputs

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ systems.append([
 ])
 
 systems.append([
-    lambda x, y, z: x ** 2 + np.sin(y) + z,
+    lambda x, y, z: x ** 2 + np.sin(y),
     lambda x, y, z: y ** 2 + np.cos(z) + x, 
     lambda x, y, z: z ** 2 + y / x + 2
 ])
 
-for i in range(len(systems)):
+for i in range(len(systems))[2:]:
     start = time.time()
     result = Solver.Jacobi(systems[i])
     end = time.time()
@@ -30,3 +30,18 @@ for i in range(len(systems)):
     print(f'MaxError: {result[1]}')
     print(f'Time: {(end - start) * 10 ** 3} ms')
     print('=' * 100)
+
+'''OUTPUT is THERE:
+    Answer: [0.73094]
+    MaxError: 0.009704890968720292
+    Time: 48953.95803451538 ms
+    ====================================================================================================
+    Answer: [-0.43667  0.00058]
+    MaxError: 0.2150171466575846
+    Time: 15633.9693069458 ms
+    ====================================================================================================
+    Answer: [ 0.00975 -0.04684  1.68808]
+    MaxError: 0.10507099066161898
+    Time: 12255.637407302856 ms
+    ====================================================================================================
+'''
