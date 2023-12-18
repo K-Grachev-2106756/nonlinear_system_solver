0b738e4a7928faf9ef88f7ad2146d3e4cad35b9b
Rostislav Lyupa <supfire2013@gmail.com>
2023-12-18 07:55:13 +0300

    implement Gauss-Seidel method

--- Solver.py
+++ Solver.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 import warnings
 warnings.simplefilter('ignore')
 
@@ -44,6 +45,90 @@ class Solver:
             while error_last > eps and iter < 1000:
                 iter += 1
                 new_values = np.array([round(values[i] - wsystem[i](values) * 0.01, 5) for i in range(n)]) # Calculating new values at i-step
+                if any(np.isnan(new_values)) or any(np.isinf(new_values)) or np.any(new_values[abs(new_values) > 1e10]): # Avoiding problems
+                    break
+                error_cur = Solver.__max_error(system, new_values) 
+                if error_cur == np.inf:
+                    break
+                if error_cur < min_error:
+                    answer, min_error = new_values, error_cur
+                if max(np.absolute(new_values - values)) < eps:
+                    break
+                error_last = error_cur
+                values = new_values
+
+            if len(answer):
+                return answer, min_error
+
+            return values, error_last
+        
+        n = len(system)
+        wsystem = [Solver.__wrapper(lam) for lam in system]        
+        if values is not None: # The result for the proposed first approximation will be returned
+            if not isinstance(values, np.ndarray): 
+                values = np.array(values)
+            return epoch(values)
+
+        answer = []
+        min_error = np.inf   
+        for step_rate in [0.025, 0.1]: # Attempts to find the optimal starting point
+            start_rate = step_rate
+            start = np.ones((n, )) + eps
+            for i in range(1000): 
+                if not i % 20:
+                    step_rate += (start_rate - (start_rate / 5) * (i // 1000))  
+                err = epoch(start)[1]
+                for j in range(n):
+                    sign = 1
+                    iter = 1
+                    while True:
+                        if not iter % 10:
+                            break
+                        start[j] += sign * step_rate
+                        ans, cur_err = epoch(start)
+                        if cur_err < eps:
+                            return ans, cur_err
+                        elif cur_err < min_error:
+                            answer, min_error = ans, cur_err
+                        if cur_err < err:
+                            err = cur_err
+                        else:
+                            if iter == 1 and cur_err > err:
+                                sign *= -1
+                                start[j] += sign * step_rate
+                            elif iter != 1 and cur_err >= err:
+                                break
+                        iter += 1
+
+        return answer, min_error
+    
+    @classmethod # simple iteration method with forward substitution
+    def GaussSeidel(cls, system: list, values=None, alpha = 0.01, eps=1e-4) -> (list, float):
+        '''
+            The simple iteration method (Jacobi method). 
+            Example:
+                Input:
+                    system = [
+                        lambda x, y, z: x ** 2 + np.sin(y) + z,
+                        lambda x, y, z: y ** 2 + np.cos(z) + x, 
+                        lambda x, y, z: z ** 2 + y / x + 2
+                    ]
+                    values = [1., 1., 1.] (if None - automatic answer search)
+                Output:
+                    ([x1, x2, x3], max_error)
+        '''
+
+        def epoch(values: np.ndarray): # Method implementation
+            answer = []
+            min_error = np.inf
+            error_last = np.inf
+            iter = 0
+            while error_last > eps and iter < 1000:
+                iter += 1
+                new_values = deepcopy(values)
+                new_values[0] = round(values[0] - wsystem[0](values) * alpha, 5)
+                for i in range(1, n):
+                    new_values[i] = round(values[i] - wsystem[i](new_values) * alpha, 5)
                 if any(np.isnan(new_values)) or any(np.isinf(new_values)) or np.any(new_values[abs(new_values) > 1e10]): # Avoiding problems
                     break
                 error_cur = Solver.__max_error(system, new_values) 
--- main.py
+++ main.py
@@ -21,27 +21,56 @@ systems.append([
     lambda x, y, z: z ** 2 + y / x + 2
 ])
 
-for i in range(len(systems))[2:]:
+for i in range(len(systems)):
     start = time.time()
     result = Solver.Jacobi(systems[i])
     end = time.time()
 
+    print('Jacobi')
+    print(f'Answer: {result[0]}')
+    print(f'MaxError: {result[1]}')
+    print(f'Time: {(end - start) * 10 ** 3} ms')
+    print('=' * 100)
+
+    start = time.time()
+    result = Solver.GaussSeidel(systems[i])
+    end = time.time()
+
+    print('Gauss-Seidel')
     print(f'Answer: {result[0]}')
     print(f'MaxError: {result[1]}')
     print(f'Time: {(end - start) * 10 ** 3} ms')
     print('=' * 100)
 
 '''OUTPUT is THERE:
+    Jacobi
     Answer: [0.73094]
     MaxError: 0.009704890968720292
-    Time: 48953.95803451538 ms
+    Time: 88267.02237129211 ms
     ====================================================================================================
+    Gauss-Seidel
+    Answer: [0.73094]
+    MaxError: 0.009704890968720292
+    Time: 96886.09671592712 ms
+    ====================================================================================================
+    Jacobi
     Answer: [-0.43667  0.00058]
     MaxError: 0.2150171466575846
-    Time: 15633.9693069458 ms
+    Time: 27429.88085746765 ms
     ====================================================================================================
+    Gauss-Seidel
+    Answer: [-0.4372   0.00164]
+    MaxError: 0.23235776146263315
+    Time: 25781.8124294281 ms
+    ====================================================================================================
+    Jacobi
     Answer: [ 0.00975 -0.04684  1.68808]
     MaxError: 0.10507099066161898
-    Time: 12255.637407302856 ms
+    Time: 20147.756814956665 ms
+    ====================================================================================================
+    Gauss-Seidel
+    Answer: [ 0.00196 -0.00897  1.61729]
+    MaxError: 0.04443646351709192
+    Time: 18273.061275482178 ms
     ====================================================================================================
 '''
